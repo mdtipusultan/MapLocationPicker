c0245c0c0c5a31e55f12f602d81b47ad5be1ff45
Md. Tipu <tipu.diu.cse@gmail.com>
2025-04-14 11:15:37 +0600

    files added
    files added

---
+++ Goggle Map/ViewController.swift
@@ -0,0 +1,77 @@
+//
+//  ViewController.swift
+//  Goggle Map
+//
+//  Created by Tipu Sultan on 4/12/25.
+
+
+import UIKit
+import GoogleMaps
+import CoreLocation
+
+class ViewController: UIViewController, GMSMapViewDelegate, CLLocationManagerDelegate {
+
+    var mapView: GMSMapView!
+    var currentMarker: GMSMarker?
+    let locationManager = CLLocationManager()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Setup Location Manager
+        locationManager.delegate = self
+        locationManager.requestWhenInUseAuthorization()
+        locationManager.startUpdatingLocation()
+
+        // Setup initial map camera
+        let camera = GMSCameraPosition.camera(withLatitude: 23.8103, longitude: 90.4125, zoom: 14.0)
+        mapView = GMSMapView.map(withFrame: self.view.bounds, camera: camera)
+        mapView.delegate = self
+        view.addSubview(mapView)
+    }
+
+    // CLLocationManagerDelegate method - this is called when the location is updated
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else { return }
+
+        // Set camera to user's current location
+        let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
+                                              longitude: location.coordinate.longitude, zoom: 14.0)
+        mapView.animate(to: camera)
+
+        // Drop a pin at current location if not already placed
+        if currentMarker == nil {
+            currentMarker = GMSMarker(position: location.coordinate)
+            currentMarker?.title = "Your Location"
+            currentMarker?.map = mapView
+            currentMarker?.isDraggable = true
+        }
+    }
+
+    // Handle any errors with location manager
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Failed to find user's location: \(error.localizedDescription)")
+    }
+
+    // When the user taps on the map
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        // Clear previous marker if any
+        currentMarker?.map = nil
+
+        // Add a new marker at tapped location
+        currentMarker = GMSMarker(position: coordinate)
+        currentMarker?.title = "Selected Location"
+        currentMarker?.map = mapView
+        currentMarker?.isDraggable = true
+
+        // Print coordinates of the tapped location
+        print("Tapped Location: Latitude: \(coordinate.latitude), Longitude: \(coordinate.longitude)")
+    }
+
+    // When the user finishes dragging the marker
+    func mapView(_ mapView: GMSMapView, didEndDragging marker: GMSMarker) {
+        let newCoordinate = marker.position
+        // Print the new location after dragging
+        print("Dragged Location: Latitude: \(newCoordinate.latitude), Longitude: \(newCoordinate.longitude)")
+    }
+}
